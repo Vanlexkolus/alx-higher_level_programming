94bc45e5d6b62cacf00fb382b9ef6a21f1e2cbaa
root <kolurejoalexanderola@gmail.com>
2023-12-04 16:28:03 -0800

    done

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
-        return None
-    if idx > len(my_list) -1:
+    if idx < 0 or idx >= len(my_list):
         #list indices start from 0, so the valid indices are 0, 1, 2, ..., len(my_list)-1. If idx is equal to the length of the list, it will be out of range.
         return None
     else:
