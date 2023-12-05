f337219eff1aca72504bbc0f092b2d8f7d995e3f
root <kolurejoalexanderola@gmail.com>
2023-12-04 18:30:02 -0800

    doen

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    for alph in my_string:
+        if alph != 'c' and alph != 'C':
+            new_string = new_string + alph
+    return new_string
+
