0474a210914327bbaf2fb6397b2c1e539650fafa
Alexander_Olamide_Kolurejo <kolurejoalexanderola@gmail.com>
2023-12-19 02:44:42 +0100

    removed the space betwee each item

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -21,7 +21,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         count = 0
         for i in range(x):
-            print(my_list[i], end=" ")
+            print(my_list[i], end="")
             count = count + 1
     except IndexError:
         pass
