67104cfe7d313403fe69628c4c88e56cc242915a
Alexander_Olamide_Kolurejo <kolurejoalexanderola@gmail.com>
2023-12-19 04:40:25 +0100

    Made each line of code pycodestyle compactable

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,17 +1,4 @@
 #!/usr/bin/python3
-'''
-Write a function that prints x elements of a list.
-where: 
-    Prototype: def safe_print_list(my_list=[], x=0):
-my_list can contain any type (integer, string, etc.)
-All elements must be printed on the same line followed by a new line.
-x represents the number of elements to print
-x can be bigger than the length of my_list
-Returns the real number of elements printed
-You have to use try: / except:
-You are not allowed to import any module
-You are not allowed to use len()
-'''
 def safe_print_list(my_list=[], x=0):
     try:
         x = int(x)
@@ -28,4 +15,3 @@ def safe_print_list(my_list=[], x=0):
     finally:
         print()
         return count
-
--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-'''
-Write a function that raises a name exception with a message.
-
-Prototype: def raise_exception_msg(message=""):
-You are not allowed to import any module
-'''
 def raise_exception_msg(message=""):
     raise NameError(message)
