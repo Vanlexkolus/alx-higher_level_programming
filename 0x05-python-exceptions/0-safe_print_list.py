975ebd44327812177a04236fddb3d12013d2ebaf
Alexander_Olamide_Kolurejo <kolurejoalexanderola@gmail.com>
2023-12-19 02:38:16 +0100

    fixed some bugs

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -15,15 +15,17 @@ You are not allowed to use len()
 def safe_print_list(my_list=[], x=0):
     try:
         x = int(x)
-    except ValueError as valError:
+    except ValueError as ValErrorr:
         print(valError)
-        return 0
-    count = 0
+
     try:
+        count = 0
         for i in range(x):
             print(my_list[i], end=" ")
             count = count + 1
-    except IndexError as IndError:
-        print(end=" ")
-    return count
+    except IndexError:
+        pass
+    finally:
+        print()
+        return count
 
