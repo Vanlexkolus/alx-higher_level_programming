be67d782767e2eef9ad066409057496b471775b6
Alex Os <alex@Alexander-LNX.myguest.virtualbox.org>
2024-01-05 02:03:20 +0100

    created a private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Created a Class name Square
+"""
+
+
+class Square:
+    """
+    the object takes in one input 'size'
+    """
+
+    def __init__(self, size):
+        """
+        made size a private attribute
+        """
+
+        self.__size = size
