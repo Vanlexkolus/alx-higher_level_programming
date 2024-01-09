090f5208cda40b1ee0e027769b136aa17bc82938
Alex Os <alex@Alexander-LNX.myguest.virtualbox.org>
2024-01-09 05:59:53 +0100

    done with it

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -11,4 +11,25 @@ def text_indentation(text):
     whenever it comes across any of the following 
     '.', '?'
     """
-    
+    if type(text) != str:
+        raise TypeError("text must be a string")
+    else:
+        r_strings = ["?", ".", ":"]
+        index = 0
+        while index < len(text):
+            if index == 0:
+                print(text[index], end="")
+            else:
+                if text[index - 1] in r_strings and text[index] == " ":
+                    index += 1
+                    continue
+                else:
+                    print(text[index], end="")
+            for val in r_strings:
+                if text[index] == val:
+                    print()
+                    print()
+            if text[index] == " ":
+                index += 1
+            else:
+                index += 1
