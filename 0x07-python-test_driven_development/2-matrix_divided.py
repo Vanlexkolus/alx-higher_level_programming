8b9294a95db7af33481918d87a0d1083d6e045e8
Alex Os <alex@Alexander-LNX.myguest.virtualbox.org>
2024-01-09 04:22:06 +0100

    wrote the Txt test and the code to print the string

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This method divides a matrix and also raises errors
+when it comes across one so it doesn't break the process
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    This method taked in two arguments, one's a
+    matrix and the other - a """
---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This is a function that prints the arguments
+in a given string
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    This function checks the input argument type and returns
+    it, if it's not a string, it raises a TypeError.
+    This function takes in two arguments (first_name and last_name)
+    """
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    elif not isinstance(last_name, (str)):
+        raise TypeError("last_name must be a string")
+    else:
+        return "My name is {} {}".format(first_name, last_name)
