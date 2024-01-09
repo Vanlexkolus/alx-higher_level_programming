6895490e7d3e2742572b8e3b7dd1abf42775e154
Alex Os <alex@Alexander-LNX.myguest.virtualbox.org>
2024-01-09 05:49:53 +0100

    created the files but I haven't written the codes yet, I wanna push it so I can get the score from the checker

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This is a text indenting function
+"""
+
+
+def text_indentation(text):
+    """
+    This function takes in a long piece of text
+    and indents it by dropping it 2 line down
+    whenever it comes across any of the following 
+    '.', '?'
+    """
+    
