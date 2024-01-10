0d80e117168cd3db03323a60d89dbca9d34dc37a
Alex Os <alex@Alexander-LNX.myguest.virtualbox.org>
2024-01-11 00:05:11 +0100

    converted py file to JSON

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This is a function that returns the JSON representation
+of an object (string)
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    this function takes in a single argument and returns
+    it as a JSON rep of a string
+    """
+    JsFile = json.dumps(my_obj)
+    print(JsFile)
