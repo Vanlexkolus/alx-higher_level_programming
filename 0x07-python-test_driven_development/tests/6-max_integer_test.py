295d55535e2af320c021c07653854d526d7a9fd1
Alex Os <alex@Alexander-LNX.myguest.virtualbox.org>
2024-01-14 00:52:33 +0100

    tried a different approach

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
+"""Unittest for max_integer([..])
 """
-This is a unittest case in the subdirectory 'tests' this
-"""
-
-
 import unittest
-max_int = __import__("6-max_integer").max_integer
+max_integer = __import__('6-max_integer').max_integer
 
+class TestMaxInteger(unittest.TestCase):
 
-class funcTest(unittest.TestCase):
     """
     A class is being declared in order to be able to run a
     unittest case, and the class inherits the unittest.TestCase
@@ -16,15 +13,15 @@ class funcTest(unittest.TestCase):
     """
 
     def test_isNon(self):
-        self.assertIsNone(max_int([]))
+        self.assertIsNone(max_integer([]))
 
     def test_isNotNone(self):
-        self.assertIsNotNone(max_int([1, 2, 3, 4, 5, 6, 7]))
+        self.assertIsNotNone(max_integer([1, 2, 3, 4, 5, 6, 7]))
 
     def test_highestInt(self):
-        Highest = max_int([1, 2, 3, 4, 5, 6, 7, 8])
+        Highest = max_integer([1, 2, 3, 4, 5, 6, 7, 8])
         self.assertEqual(Highest, 8)
 
     def test_IsAnInt(self):
-        dataType = max_int([1, 76, 4, 6])
+        dataType = max_integer([1, 76, 4, 6])
         self.assertTrue(type(dataType) == int)
