f1a6dd1a1957c84a6297a13e077b1ecb4935f79a
Alex Os <alex@Alexander-LNX.myguest.virtualbox.org>
2024-01-13 15:41:21 +0100

    Has too many processed to remember

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import unittest
+max_int = __import__ ("6-max_integer").max_integer
+
+class funcTest(unittest.TestCase):
+
+    def test_isNon(self):
+        self.assertIsNone(max_int([]))
+        return None
+    
+    def test_isNotNone(self):
+        self.assertIsNotNone(max_int([1, 2, 3, 4, 5, 6, 7]))
+    
+    def test_highestInt(self):
+        Highest = max_int([1, 2, 3, 4, 5, 6, 7, 8])
+        self.assertEqual(Highest, 8)
