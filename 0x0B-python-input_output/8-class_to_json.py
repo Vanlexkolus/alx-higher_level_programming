2eba52130fd993fba52c5bb24ec9e0c3a964079b
Alex Os <alex@Alexander-LNX.myguest.virtualbox.org>
2024-01-13 03:02:04 +0100

    this one's a tough one

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This function returns  dictionary description
+for JSON serialization of an object
+"""
+
+
+def class_to_json(obj):
+    """
+    Returns the dictionary description with a simple data structure
+    for JSON serialization of an object.
+    """
+    obj_dictionary = obj.__dict__
+
+    if hasattr(obj, "__class__"):
+        obj_dictionary["__class__"] = obj.__class__.__name__
+    for key, value in obj_dictionary.items():
+        if hasattr(value, "__dict__"):
+            obj_dictionary[key] = value.__dict__
+    return obj_dictionary
