55f2873587626d1ec6aa0542f6fc19b0e085d8a4
Alex Os <alex@Alexander-LNX.myguest.virtualbox.org>
2024-01-14 01:03:00 +0100

    added more test cases

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -12,16 +12,37 @@ class TestMaxInteger(unittest.TestCase):
     function
     """
 
-    def test_isNon(self):
-        self.assertIsNone(max_integer([]))
+    def test_module_docstring(self):
+        moduleDoc = __import__('6-max_integer').__doc__
+        self.assertTrue(len(moduleDoc) > 1)
 
-    def test_isNotNone(self):
-        self.assertIsNotNone(max_integer([1, 2, 3, 4, 5, 6, 7]))
+    def test_function_docstring(self):
+        functionDoc = __import__('6-max_integer').max_integer.__doc__
+        self.assertTrue(len(functionDoc) > 1)
 
-    def test_highestInt(self):
-        Highest = max_integer([1, 2, 3, 4, 5, 6, 7, 8])
-        self.assertEqual(Highest, 8)
+    def test_signed_unsigned_and_float(self):
+        self.assertEqual(max_integer([1, 3, 4, 6]), 6)
+        self.assertEqual(max_integer([0]), 0)
+        self.assertEqual(max_integer([2, 0, -1]), 2)
+        self.assertEqual(max_integer([0.5, 1.5, 2.5]), 2.5)
+        self.assertEqual(max_integer([1, 2, 0.5, -1]), 2)
 
-    def test_IsAnInt(self):
-        dataType = max_integer([1, 76, 4, 6])
-        self.assertTrue(type(dataType) == int)
+    def test_list(self):
+        self.assertEqual(max_integer([[1, 2], [3, 8]]), [3, 8])
+
+    def test_string(self):
+        self.assertEqual(max_integer("abcd"), 'd')
+        self.assertEqual(max_integer(['a', 'j', 'l', 'q']), 'q')
+        self.assertEqual(max_integer("gbdf9"), 'g')
+
+    def test_other_data_type(self):
+        with self.assertRaises(TypeError):
+            max_integer({1, 2, 4})
+        with self.assertRaises(TypeError):
+            max_integer({1, 3, 5}, {1, 4, 6})
+        with self.assertRaises(TypeError):
+            max_integer(None)
+        with self.assertRaises(TypeError):
+            max_integer(True)
+        with self.assertRaises(TypeError):
+            max_integer([1, "sdf", {1, 4, 5}])
