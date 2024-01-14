93dea742f195cfd64d43712ea89e16abe01b724b
Alex Os <alex@Alexander-LNX.myguest.virtualbox.org>
2024-01-14 17:36:33 +0100

    added comments to the print_sorted function

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,11 +4,17 @@ This module is of a class MyList that inherits
 'list'
 """
 
+
 class MyList(list):
     """
     This is a class that inherits a built-in
     class name 'list' and has different methods
     """
     def print_sorted(self):
+        """
+        This method uses the 'sorted' function present
+        in the inherited class 'list' to sort the ints
+        from lowest to highest
+        """
         sorted_list = sorted(self)
         print(sorted_list)
