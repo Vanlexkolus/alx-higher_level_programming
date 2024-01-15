233184a75f13578f32e40aa758fc79819e9acc32
Alex Os <alex@Alexander-LNX.myguest.virtualbox.org>
2024-01-15 03:12:47 +0100

    removed a few whitespaces

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-A class Rectangle that inherits from BaseGeometry 
+A class Rectangle that inherits from BaseGeometry
 """
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
@@ -15,7 +15,7 @@ class Rectangle(BaseGeometry):
         This init method takes in two args and it helps me with
         the project: Write a class Rectangle that inherits from BaseGeometry
         (7-base_geometry.py). (task based on 8-rectangle.py)
-        Instantiation with width and height: 
+        Instantiation with width and height:
         def __init__(self, width, height):
         width and height must be private. No getter or setter
         width and height must be positive integers validated by
