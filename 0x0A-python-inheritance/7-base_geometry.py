1a99e9b6d8caa7729722e0c67411fc7a28e83fcb
Alex Os <alex@Alexander-LNX.myguest.virtualbox.org>
2024-01-15 01:52:21 +0100

    edited the error message

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -24,6 +24,6 @@ class BaseGeometry:
         self.name = name
         self.value = value
         if type(value) != int:
-            raise TypeError("<name> must be an integer")
+            raise TypeError("name must be an integer")
         if value <= 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError("name must be greater than 0")
