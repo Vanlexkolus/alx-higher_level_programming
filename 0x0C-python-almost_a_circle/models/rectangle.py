1bd764213e22a910bc9cb9a8633382528bb0e05e
Alex Os <alex@Alexander-LNX.myguest.virtualbox.org>
2024-01-16 02:55:08 +0100

    Did a lot of reasoning to get here, hope it checks

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""
+This is a module containing a class: Rectangle that
+inherits the class: Base
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """
+    This is a class that inherits the methods
+    of the class base
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        This init method takes in 5 arguments
+        """
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def set_width(self, new_wid):
+        if not isinstance(new_wid, int):
+            raise TypeError("Not an int")
+        if new_wid <= 0:
+            raise ValueError("Must be greater than 0")
+        self.__width = new_wid
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def set_height(self, newH_val):
+        self.__height = newH_val
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def set_x(self, x_val):
+        if not isinstance(x_val, int):
+            raise TypeError("Not an int")
+        if x_val <= 0:
+            raise ValueError("Must be > 0")
+        self.__x = x_val
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def set_y(self, y_val):
+        self.__y = y_val
