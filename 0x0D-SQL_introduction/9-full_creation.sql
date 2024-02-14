0477e20fcebeb56c4ac59e4fd51fe4280325e35c
Alexander olamide Kolurejo <Kolurejoalexanderola@gmail.com>
2024-02-14 03:17:44 +0100

    minor edit

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -6,7 +6,4 @@ CREATE TABLE IF NOT EXISTS second_table (
     score INT
 );
 
-INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
-INSERT INTO second_table (id, name, score) VALUES (2,  'Alex', 3);
-INSERT INTO second_table (id, name, score) VALUES (3,  'Bob', 4);
-INSERT INTO second_table (id, name, score) VALUES (4,  'George', 8);
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10), (2,  'Alex', 3), (3,  'Bob', 4), (4,  'George', 8);
