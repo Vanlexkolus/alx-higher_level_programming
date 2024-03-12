007f4ba3af7d262a874f48bda2762f1c99d6f892
Alexander olamide Kolurejo <Kolurejoalexanderola@gmail.com>
2024-03-12 02:09:09 +0100

    made use of undefined since the task said I shouldn't use .length

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 //  This script print's the arguments that are being passed to it
-const myArg = process.argv.length;
-const i = 2;
-if (myArg <= 2) {
+if (process.argv[2] == undefined) {
   console.log('No argument');
-} else if (myArg > 2) {
-  console.log(process.argv[i]);
+} else {
+  console.log(process.argv[2]);
 }
