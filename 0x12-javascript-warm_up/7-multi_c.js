268b499d8b4392e42b53703eb5b5f38e2961e98d
Alexander olamide Kolurejo <Kolurejoalexanderola@gmail.com>
2024-03-12 03:49:45 +0100

    done and working fine

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+/*  Write a script that prints x times “C is fun”
+
+    Where x is the first argument of the script
+    If the first argument can’t be converted to an integer, print “Missing number of occurrences”
+    You must use console.log(...) to print all output
+    You are not allowed to use var
+    You can use only two console.log
+    You must use a loop (while, for, etc.)
+*/
+const multiple = parseInt(process.argv[2]);
+let index = 0;
+if (isNaN(multiple)) {
+  console.log('Missing number of occurrences');
+} else {
+    while (multiple > index) {
+      console.log('C is fun');
+      index++;
+    }
+}
