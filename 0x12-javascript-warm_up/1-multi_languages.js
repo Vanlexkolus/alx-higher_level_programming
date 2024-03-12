6a8d4c27bbb85763cdec3e0965b4e2261c959395
Alexander olamide Kolurejo <Kolurejoalexanderola@gmail.com>
2024-03-12 01:25:15 +0100

    done with task 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+//  this code prints three lines of strings
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+while (myVar.length > i) {
+  console.log(myVar[i]);
+  i++;
+}
