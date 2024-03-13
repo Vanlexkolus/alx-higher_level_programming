db8ba256eae9d9191fffe0889fb4615d52591f51
Alexander olamide Kolurejo <Kolurejoalexanderola@gmail.com>
2024-03-13 02:39:21 +0100

    done

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -20,11 +20,7 @@ class Rectangle {
 
   print () {
     for (let i = 0; i < this.height; i++) {
-      let k = '';
-      for (let l = 0; l < this.width; l++) {
-        k = k + 'X';
-      }
-      console.log(k);
+      console.log('X'.repeat(this.width));
     }
   }
 
