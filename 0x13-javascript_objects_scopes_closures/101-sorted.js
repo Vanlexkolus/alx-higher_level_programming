5cb7670412c83247afdbc4aca0ebac0cc6d01b5c
Alexander olamide Kolurejo <Kolurejoalexanderola@gmail.com>
2024-03-13 03:59:53 +0100

    done

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+/*  Write a script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence.
+
+    Your script must import dict from the file 101-data.js
+    In the new dictionary:
+        A key is a number of occurrences
+        A value is the list of user ids
+    Print the new dictionary at the end
+*/
+const { dict } = require('./101-data.js');
+const Dictn = {};
+for (const N in dict) {
+  if (Dictn[dict[N]] === undefined) {
+    Dictn[dict[N]] = [];
+  }
+  Dictn[dict[N]].push(N);
+}
+console.log(Dictn);
