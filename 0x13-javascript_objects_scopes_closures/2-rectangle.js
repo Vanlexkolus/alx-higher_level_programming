f6f2b730386c7d01e453322aefad5ff94efdd88e
Alexander olamide Kolurejo <Kolurejoalexanderola@gmail.com>
2024-03-13 01:45:00 +0100

    done

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -9,9 +9,7 @@
 */
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
-      console.log();
-    } else {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
