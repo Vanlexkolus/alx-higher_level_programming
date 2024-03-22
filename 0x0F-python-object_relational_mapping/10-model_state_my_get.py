4426a643d889002c3826a202acf7b1865f3e54af
Alexander olamide Kolurejo <Kolurejoalexanderola@gmail.com>
2024-03-22 13:56:38 +0100

    done

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This is a script thatg get State objects that have 'a' from the database hbtn_0e_6_usa
+"""
+
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1],
+        argv[2],
+        argv[3]),
+        pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Get first state object from database
+    state_name = argv[4]
+    states = session.query(State).filter(
+            State.name == state_name).first()
+
+    if states:
+        print("{:d}".format(states.id))
+    else:
+        print("Not found")
+
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This is a script thatg get State objects that have 'a' from the database hbtn_0e_6_usa
+"""
+
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1],
+        argv[2],
+        argv[3]),
+        pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Get first state object from database
+    states = session.query(State).filter(
+            State.name.like('%a%')).order_by(State.id)
+
+    for state in states:
+        print("{:d}: {:s}".format(state.id, state.name))
+
+    session.close()
