47779e6c43e7220ab7bffe10b0756a92cb489468
Alexander olamide Kolurejo <Kolurejoalexanderola@gmail.com>
2024-03-22 14:11:40 +0100

    fixed

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,32 +1,21 @@
 #!/usr/bin/python3
 """
-This is a script thatg get State objects that have 'a'
-from the database hbtn_0e_6_usa
+All states via SQLAlchemy
 """
-
-from sqlalchemy import create_engine
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
 from sys import argv
-
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1],
-        argv[2],
-        argv[3]),
-        pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-    # Get first state object from database
-    state_name = argv[3]
-    states = session.query(State).filter(
-            State.name == state_name).first()
-
-    if states:
-        print("{:d}".format(states.id))
-    else:
-        print("Not found")
-
+    session = Session(engine)
+    new_obj = State(name='Louisiana')
+    session.add(new_obj)
+    session.commit()
+    print(new_obj.id)
     session.close()
