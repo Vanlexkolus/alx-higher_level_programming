4709566359ddc23f9fa04e471d65d2208def5b92
Alexander olamide Kolurejo <Kolurejoalexanderola@gmail.com>
2024-03-28 17:57:14 +0100

    done

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-
-# Check if the URL argument is provided
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send a request to the URL and store the response in a variable
-response=$(curl -s -w "%{size_download}" -o /dev/null "$1")
-
-# Display the size of the response body in bytes
-echo "$response"
+#	Takes a URL, sends a request to that URL, and displays the size of the body of the response
+curl -s $1 | wc -c
 
